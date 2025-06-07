6221a1784db73d2aa6daa654b4e0583c6bea695a
Eve <ridwan.native@gmail.com>
2025-06-07 22:40:49 +0700

    update key

--- lib/core/common/constants.dart
+++ lib/core/common/constants.dart
@@ -1,6 +1,6 @@
 abstract class Constants {
   static String baseUrl = 'https://newsapi.org/v2/';
-  static String apiKey = 'c2177d86f19a48eba585fe2e67612478';
+  static String apiKey = '';
   static String sources = 'sources';
   static String topHeadline = 'top-headlines';
   static String cacheSources = 'sources_cache';
